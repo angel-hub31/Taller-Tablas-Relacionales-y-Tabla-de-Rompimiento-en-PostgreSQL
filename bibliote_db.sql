7eb7be6cc83414f8f28ea1bcbd82cb62daa10778
angel-hub31 <moralesmales1@hotmail.com>
2026-05-24 17:37:15 -0500

    se crea la tabla autores

--- bibliote_db.sql
+++ bibliote_db.sql
@@ -3,3 +3,10 @@ codigo varchar (5) primary key,
 titulo varchar (50) not null,
 paginas int not null
 );
+
+
+create table autores(
+id int primary key,
+nombre varchar (50) not null,
+pais varchar (30) not null
+);
